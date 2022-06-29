4508734567c9a3e788d2598fb276eb4f1f427dae
KibetAmon <kibetngetich805@gmail.com>
2022-06-29 23:29:09 +0300

    Introduced a data transport object

--- BankingApp/src/main/java/com/AmonKibet/BankingApp/controller/TransactionController.java
+++ BankingApp/src/main/java/com/AmonKibet/BankingApp/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.AmonKibet.BankingApp.controller;
 
 
+import com.AmonKibet.BankingApp.controller.dto.TransactionDto;
 import com.AmonKibet.BankingApp.transaction.Transaction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.AmonKibet.BankingApp.transactionservice.TransactionService;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
@@ -19,8 +23,18 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
     @GetMapping("/{accountNumber}")
-    public List<Transaction> findAllByAccountNumber(@PathVariable("accountNumber") final Integer accountNumber){
-        return transactionService.findAllByAccountNumber(accountNumber);
+    public List<TransactionDto> findAllByAccountNumber(@PathVariable("accountNumber") final Integer accountNumber){
+        return transactionService.findAllByAccountNumber(accountNumber)
+
+                .stream().map(this::map) //returns a stream consisting of the results of applying the
+                // map function to the elements of this stream
+
+                .collect(toList()); //It returns a Collector Interface that
+        // gathers the input data onto a new list.
+    }
+
+    private TransactionDto map(final Transaction tr){
+        return TransactionDto.apply(tr);
     }
     private final TransactionService transactionService;
 
